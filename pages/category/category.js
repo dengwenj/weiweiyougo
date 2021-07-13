17b95b817a1ca06629ea40d7f7fd320f8850e85e
xiaodeng <927561237@qq.com>
2021-07-13 19:56:56 +0800

    使用缓存技术

--- pages/category/category.js
+++ pages/category/category.js
@@ -19,14 +19,41 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function() {
-    // 发送请求
-    this._getCategory()
+    /* 
+      使用缓存技术
+      1 先判断本地存储中有没有旧的数据
+      2 没有旧的数据 直接发送请求
+      3 有旧的数据 同时旧的数据没有过期 就使用 本地存储中的旧数据
+    */
+    const storage = wx.getStorageSync('cates')
+    if (!storage) {
+      // 发送请求
+      this._getCategory()
+    } else {
+      // 旧的数据没有过期 就使用旧的数据 旧的数据过期了就重新发送请求
+      if (Date.now() - storage.time > 1000 * 300) {
+        // 就从新发送请求
+        this._getCategory()
+      } else {
+        // 没有过期 就是使用旧的数据 渲染页面
+        this.Cates = storage.data
+        const leftMenuList = this.Cates.map(item => item.cat_name)
+        const rightContent = this.Cates[0].children
+        this.setData({
+          leftMenuList,
+          rightContent
+        })
+      }
+    }
   },
 
   async _getCategory() {
     const { data } = await getCategory()
-    console.log(data);
     this.Cates = data.message
+
+    // 就把这个数据存起来
+    // 这种格式 {time:Date.now(),data:...}
+    wx.setStorageSync('cates', { time: Date.now(), data: this.Cates })
 
     const leftMenuList = this.Cates.map((item) => item.cat_name)
     const rightContent = this.Cates[0].children
