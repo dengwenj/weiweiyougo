1863a84289460522b669266e6417083a63dd3da6
xiaodeng <927561237@qq.com>
2021-07-13 21:41:43 +0800

    tabbar组件封装

--- pages/goods_list/goods_list.js
+++ pages/goods_list/goods_list.js
@@ -5,7 +5,19 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    tabs: [{
+      id: 0,
+      name: '综合',
+      isActive: true,
+    }, {
+      id: 1,
+      name: '销量',
+      isActive: false,
+    }, {
+      id: 0,
+      name: '价格',
+      isActive: false,
+    }]
   },
 
   /**
@@ -15,52 +27,15 @@ Page({
     console.log(options);
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function() {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function() {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function() {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function() {
-
+  // 自定义事件 子传父
+  ItemTap(e) {
+    const detail = e.detail
+    const { tabs } = this.data
+    tabs.forEach((item, index) => {
+      index === detail ? item.isActive = true : item.isActive = false
+    })
+    this.setData({
+      tabs
+    })
   }
 })
