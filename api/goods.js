7f94f52c458d78882645c5bd59895f9153ad806c
xiaodeng <927561237@qq.com>
2021-07-14 14:12:12 +0800

    获取商品详情数据

--- api/goods.js
+++ api/goods.js
@@ -1,8 +1,18 @@
 import request from '../request/request'
 
+// 商品列表
 export const goodsSearch = data => {
   return request({
     url: '/goods/search',
+    method: 'GET',
+    data
+  })
+}
+
+// 商品详情
+export const getGoodsDetail = data => {
+  return request({
+    url: '/goods/detail',
     method: 'GET',
     data
   })
--- pages/goods_detail/goods_detail.js
+++ pages/goods_detail/goods_detail.js
@@ -1,66 +1,28 @@
-// pages/goods_detail/goods_detail.js
+import { getGoodsDetail } from "../../api/goods"
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    goodsDetail: {} // 商品详情 
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-
+  onLoad: function(options) {
+    const { goods_id } = options
+    this._getGoodsDetail(goods_id)
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  // 获取商品详情数据
+  async _getGoodsDetail(goods_id) {
+    const res = await getGoodsDetail({ goods_id })
+    const goodsDetail = res.data.message
+    this.setData({
+      goodsDetail
+    })
   }
 })
