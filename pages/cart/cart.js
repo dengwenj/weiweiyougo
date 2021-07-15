7416e44ff83fe6c7525ee548a6e6732506ded1c4
xiaodeng <927561237@qq.com>
2021-07-15 15:24:57 +0800

    收货地址和按钮切换显示

--- pages/cart/cart.js
+++ pages/cart/cart.js
@@ -5,62 +5,36 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    dizhi: {} // 获取用户地址
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
 
   },
+  onShow() {
+    // 当页面显示的时候获取用户的收货地址
+    const dizhi = wx.getStorageSync('address')
+    if (dizhi && dizhi.userName) {
+      dizhi.all = `${dizhi.provinceName}${dizhi.cityName}${dizhi.countyName}${dizhi.detailInfo}`
+    }
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+    // 把地址赋值给 data 中
+    this.setData({
+      dizhi
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  handleChooseAddress() {
+    // 调用小程序内置 api 获取用户的收货地址
+    wx.chooseAddress({
+      success: (result) => {
+        // 获取了用户的收货地址就把存到本地存储中
+        wx.setStorageSync('address', result);
+      }
+    })
   }
+
 })
