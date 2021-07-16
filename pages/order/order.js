65050bf7a2878829b11a614f7fed4d3de08edb7a
xiaodeng <927561237@qq.com>
2021-07-16 21:59:14 +0800

    引入tabs组件

--- pages/order/order.js
+++ pages/order/order.js
@@ -5,62 +5,33 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    tabs: [{
+      id: 0,
+      name: '全部',
+      isActive: true
+    }, {
+      id: 1,
+      name: '待付款',
+      isActive: false
+    }, {
+      id: 2,
+      name: '待发货',
+      isActive: false
+    }, {
+      id: 3,
+      name: '退款/退货',
+      isActive: false
+    }, ]
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  handleItemTap(e) {
+    const { detail } = e
+    const { tabs } = this.data
+    tabs.forEach((item, index) => {
+      detail === index ? item.isActive = true : item.isActive = false
+    })
+    this.setData({
+      tabs
+    })
   }
 })
